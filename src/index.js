f610506bdfb763aa8938cc2b9972505a3fe6da3b
Sneha-Singh21 <indsneha2002@gmail.com>
2024-03-09 07:13:55 +0530

    db connection setup done

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// but this syntax is not matching with our default syntaxt and is not consistent
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+// This is one waya for writing databse connection code
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR :", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR :", error);
+        throw error
+    }
+})()
+*/
