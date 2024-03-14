a98a10019b909f3fe221c4a7a80d4617720706da
Sneha-Singh21 <indsneha2002@gmail.com>
2024-03-14 23:32:34 +0530

    utils codes discussion

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// whenever we use a middleware or want to do a configuration setting we use it through app.use()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// cookieParser is basically use when we want to access users browser from our server and also 
+// set its cookies i.e., to perform CRUD operations on users browser
+app.use(cookieParser())
+
+export { app }
